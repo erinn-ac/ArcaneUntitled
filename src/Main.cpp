02ffbef960d2a862463d047f3a096ff40178decd
Jayden Vi <jaydenvi2006@hotmail.com>
2026-06-23 14:24:29 +0100

    modified:   CMakeLists.txt 	new file:   dependencies/KHR/khrplatform.h 	new file:   dependencies/glad/glad.h 	modified:   src/Main.cpp 	modified:   src/config.h 	new file:   src/glad.c 	new file:   src/shaders/testFile.txt

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,15 @@
 
 int main() {
 
+  std::ifstream file;
+  std::string line;
+
+  file.open(std::string(PROJECT_ROOT_DIR) + "/src/shaders/testFile.txt");
+  std::cout << file.is_open() << std::endl;
+  while (std::getline(file, line)) {      
+    std::cout << line << std::endl;
+  }
+  file.close();
   GLFWwindow* window;
 
   if (!glfwInit()) {
@@ -11,10 +20,23 @@ int main() {
 
   window = glfwCreateWindow(800, 600, "My Window", NULL, NULL);
 
+  glfwMakeContextCurrent(window);
+  
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
   while(!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
   }
+
   glfwTerminate();
 
   return 0;
