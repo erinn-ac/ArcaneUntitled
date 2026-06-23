a646636cf9d0e7ff770a822243776b8352a5e4ed
Jayden Vi <jaydenvi2006@hotmail.com>
2026-06-23 12:16:17 +0100

    modified:   CMakeLists.txt 	modified:   CMakePresets.json 	new file:   cmake/Findglfw3.cmake 	new file:   dependencies/GLFW/glfw3.h 	new file:   dependencies/GLFW/glfw3native.h 	modified:   src/Main.cpp 	new file:   src/config.h

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,21 @@
-#include <iostream>
-using namespace std;
+#include "config.h"
 
 int main() {
-  cout << "hello guppy boy!";
-  cout << "Hello World!";
+
+  GLFWwindow* window;
+
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
+
+  window = glfwCreateWindow(800, 600, "My Window", NULL, NULL);
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+  }
+  glfwTerminate();
+
   return 0;
 }
